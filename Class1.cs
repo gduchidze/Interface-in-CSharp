8fcbed48c0fca3d9695e980fc783ce0b149fbbf0
giorg <giorg@DUCH>
2024-01-01 22:51:44 +0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    public interface InterfaceCube
+    {
+        int cube();
+    }
+    public interface InterfaceSquare
+    { 
+        int square();
+    }
+    internal class Class1
+    {
+        public int x_1;
+        public Class1(int x)
+        { 
+            x_1 = x;
+        }
+
+    }
+    public class Inter : InterfaceSquare, InterfaceCube
+    {
+        public int x_1;
+        public Inter(int x_1)
+        {
+            this.x_1 = x_1;
+        }
+
+        public int cube()
+        {
+            return x_1 * x_1 * x_1;
+        }
+        public int square()
+        {
+            return x_1 * x_1;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int x1 = int.Parse(textBox1.Text);
+            Inter obj = new Inter(x1);
+            int square = obj.square();
+            int cube = obj.cube();
+            label1.Text = "რიცხვის კვადრატი = " + square.ToString();
+            label2.Text = "რიცხვის კუბი = " + cube.ToString();
+
+
+        }
+    }
+}
